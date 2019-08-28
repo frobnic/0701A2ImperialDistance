72084520edf06685c5f5158a0ae829df5ac6f8b8
frank paulsen <frobnic+ita@gmail.com>
2019-08-28 10:54:21 +0200

    Projektdateien hinzufügen.

---
+++ 0701A2ImperialDistance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0701A2ImperialDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
