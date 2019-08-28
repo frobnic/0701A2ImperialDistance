5b826e37a86baf5b793559df2362835b220c0102
frank paulsen <frobnic+ita@gmail.com>
2019-08-28 11:02:21 +0200

    code inserted

--- 0701A2ImperialDistance/Program.cs
+++ 0701A2ImperialDistance/Program.cs
@@ -8,8 +8,19 @@ namespace _0701A2ImperialDistance
 {
     class Program
     {
+        static int MetricToIdiotic(int metric)
+        {
+            return ((int)((float)metric / 1.609344));
+        }
+
         static void Main(string[] args)
         {
+            int metricdistance;
+
+            Console.Write("Bitte geben Sie eine Entfernung in vernünftigen Einheiten an: ");
+            if (int.TryParse(Console.ReadLine(), out metricdistance))
+                Console.WriteLine("Das entspricht etwa {0} komischen Einheiten!", MetricToIdiotic(metricdistance));
+            Console.WriteLine();
         }
     }
 }
